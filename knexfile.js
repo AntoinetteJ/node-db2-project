1a95fe5be1edd922c692aa5b47ca3ccff34ea220
Antoinette <create@aplusmedia.info>
2020-05-29 20:37:03 -0400

    endpoints are working

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,19 @@
-module.exports ={
-    development:{
-        client: 'sqlite3',
-        connection:{
-            filename: '/data/car-dealer.db3'
-        }
-    }
-}
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './car_dealer.db3'
+    },
+    migration: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+  },
+  useNullAsDefault: true
+},
+
+};
